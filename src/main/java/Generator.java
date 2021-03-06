de9f614920f30e92e126150874be783a131e2151
Hovhannes23 <twinkle.groupbz@gmail.com>
2021-03-06 19:00:27 +0300

    javadocs created and done

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -2,8 +2,15 @@ import java.io.*;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+
 public class Generator {
 
+    /**
+     * Method generates file with random content
+     * @param countOfLines  - count of lines in generatedFile
+     * @param maxLengthOfLine  - max length of line
+     * @return  generated file
+     */
     public static File generateFile(int countOfLines, int maxLengthOfLine) {
         File generatedFile = new File("generatedFile.txt"); // create file to which we`ll write generated text
 
@@ -11,14 +18,15 @@ public class Generator {
 
        try(Writer out=new BufferedWriter(new FileWriter(generatedFile));) { // create writer which will write text to our generated file
 
-           for (int i = 0; i < countOfLines; i++) {   // in this loop we randomly choose letters and concat them to String. Count of letters in one line is from 1 to maxLengthOfLine.
+           for (int i = 0; i < countOfLines; i++) {   // in this loop we randomly choose letters and concat them to StringBuilder.
+                                                      // Count of letters in one line is from 1 to maxLengthOfLine.
                StringBuilder res =
                         new StringBuilder(new Random().ints(new Random().nextInt(maxLengthOfLine) + 1, 0, chars.length)
                                 // randomly choose ints from 0 to 51. Work via StringBuilder in order to use memory efficiently
                        .mapToObj(c -> String.valueOf(chars[c])) // from char[] we get char by ints and convert its to String
                        .collect(Collectors.joining())); // then concat them to String
                System.out.println(res);
-               out.append(res); // write String to generatedFile
+               out.append(res); // write StringBuilder to generatedFile
                out.append("\n");
            }
        }
@@ -27,7 +35,6 @@ public class Generator {
         }
 
         return generatedFile;
-
 
     }
 }
--- src/main/java/MergeSort.java
+++ src/main/java/MergeSort.java
@@ -1,12 +1,16 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MergeSort {
 
-    public static List<String> fileToList (File inputFile) throws FileNotFoundException { // метод считывает строки из файла, записывает их в List и возвращает его
+/**
+ * method reads lines from file, write them to list and returns that list
+ * @param inputFile - from this file we read lines
+ * @return lines
+ */
+    public static List<String> fileToList (File inputFile) throws FileNotFoundException {
 
         List<String> lines = new ArrayList<>();
         Scanner inputFileScanner= new Scanner(inputFile);
@@ -18,7 +22,14 @@ public class MergeSort {
         return lines;
     }
 
-    public static List<String> divideList(List<String> inputList){
+
+    /**
+     * @param inputList - method divides this list recursively till it divides.
+     *                  And then merge and sort lists
+     * @return sortedList
+     * Method also write sorted list to file
+     */
+    public static List<String> divideAndSortList(List<String> inputList){
 
         if (inputList==null)
             return null;
@@ -26,41 +37,67 @@ public class MergeSort {
         if(inputList.size()<2)
             return inputList;
 
-        List<String> listL = new ArrayList<>();
-        List<String> listR = new ArrayList<>();
-
-
+        List<String> listL;
+        List<String> listR;
 
         listL = inputList.subList(0,inputList.size()/2);
         listR = inputList.subList(inputList.size()/2,inputList.size());
 
-        listL = divideList(listL);
-        listR = divideList(listR);
+        listL = divideAndSortList(listL);
+        listR = divideAndSortList(listR);
 
-       return sortList(listL,listR);
+        List<String> sortedList = mergeList(listL,listR);
+
+        writeListToFile(sortedList);
+
+       return sortedList;
 
     }
 
 
-    public static List<String> sortList(List<String> listL, List<String>listR){  // метод 2 сортированных списка слияет и сортирует
+    /**
+     * @param list - method writes to file content of this list
+     */
+    public static void writeListToFile(List<String> list){
 
-        int indexL=0;
-        int indexR=0;
+        try (Writer out = new BufferedWriter(new FileWriter("sortedFile.txt"))) {
+            for (String s : list) {
+                out.append(s);
+                out.append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * @param listL  - left sorted list
+     * @param listR - right sorted list
+     * @return sortedList
+     * Method merge and sort two sorted lists
+     */
+
+    public static List<String> mergeList(List<String> listL, List<String>listR){
+
+        int indexL=0; // current index of listL
+        int indexR=0; // current index of listR
 
         List<String> sortedList = new ArrayList<>();
 
         for(int i=0;i<(listL.size()+listR.size());i++) {
 
-             if (indexL==listL.size()) {
+             if (indexL==listL.size()) {    // if there is no elements in listL, we add to sortedList elements of listR
                  sortedList.add(listR.get(indexR));
                  indexR++;
              }
 
-             else if (indexR==listR.size()) {
+             else if (indexR==listR.size()) {  // if there is no elements in listR, we add to sortedList elements of listL
                  sortedList.add(listL.get(indexL));
                  indexL++;
              }
-             else if ((listL.get(indexL).compareTo(listR.get(indexR)) <= 0)) {
+             else if ((listL.get(indexL).compareTo(listR.get(indexR)) <= 0)) {  // compare two elements from listL and listR, and
+                                                                               //  write smaller element to sortedList.
                 sortedList.add(listL.get(indexL));
                 indexL++;
             }
